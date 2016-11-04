5ee80342a4abef5349cfd945ef9ef1477688111f
Víctor Fernández Rico <vfrico@gmail.com>
2016-11-04 13:25:24 +0100

    Changed broker and backend of Celery from RabbitMQ to Redis

--- rest-service/async_server/celery.py
+++ rest-service/async_server/celery.py
@@ -1,14 +1,20 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
+import os
+
+port = os.environ['REDIS_PORT_6379_TCP_PORT']
+host = os.environ['REDIS_PORT_6379_TCP_ADDR']
+redis_uri = "redis://{0}:{1}/0".format(host, port)
+rabbit_mq = 'amqp://'
 
 app = Celery('async_server',
-             broker='amqp://',
-             backend='amqp://',
+             broker=redis_uri,
+             backend=redis_uri,
              include=['async_server.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    result_expires=None,        # Celery will keep values forever
+    result_expires=None,        # Celery will keep values forever TODO
     task_track_started=True,    # Tasks will show started status
 )
 
