a69ad9d4aa8a98103d0f76088ed23d7f9ac8fadc
Víctor Fernández Rico <vfrico@gmail.com>
2016-11-17 11:18:06 +0100

    Adding license for new files

--- rest-service/dataset_dto.py
+++ rest-service/dataset_dto.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# coding:utf-8
+#
+# dataset_dto.py: Contains the DTO for Datasets
+# Copyright (C) 2016  Víctor Fernández Rico <vfrico@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import data_access_base
 import kgeserver.dataset as dataset
