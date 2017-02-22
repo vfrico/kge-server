2d475573f020b2af94ef1c70f8f414d4c75b60af
Víctor Fernández Rico <vfrico@gmail.com>
2017-02-22 12:43:37 +0100

    Added CORS support for DELETE (and other) HTTP methods

--- rest-service/routes.py
+++ rest-service/routes.py
@@ -41,7 +41,8 @@ from endpoints.algorithms import AlgorithmFactory, AlgorithmResource
 from endpoints.tasks import TasksResource
 
 # CORS
-cors = CORS(allow_all_origins=True, allow_all_headers=True)
+cors = CORS(allow_all_origins=True, allow_all_headers=True,
+            allow_all_methods=True)
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API(middleware=[cors.middleware])
