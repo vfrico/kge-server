96b09817a2e0f886c9f7bc187c850c9382e42f0f
Víctor Fernández Rico <vfrico@gmail.com>
2017-02-23 11:56:24 +0100

    Add CORS suppport for all headers

--- rest-service/routes.py
+++ rest-service/routes.py
@@ -42,7 +42,7 @@ from endpoints.tasks import TasksResource
 
 # CORS
 cors = CORS(allow_all_origins=True, allow_all_headers=True,
-            allow_all_methods=True)
+            allow_all_methods=True, expose_headers_list=['*'])
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API(middleware=[cors.middleware])
