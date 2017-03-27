cf24b28049bff2f4bcbf3d1c910595480e39b075
Víctor Fernández Rico <vfrico@gmail.com>
2017-03-27 13:10:30 +0200

    Changed docker images

--- images/celery/launch_celery.sh
+++ images/celery/launch_celery.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-echo "RUNNING CELERY"
+echo "*** RUNNING CELERY"
 CODE_PATH="/home/$CONDA_USER/kge-server"
 cd $CODE_PATH
+
+echo "*** Installing deps"
 python3 setup.py install
+echo "*** end installing"
+
 WORKING_DIR_EX="$CODE_PATH/rest-service/"
 cd $WORKING_DIR_EX
 echo "$WORKING_DIR_EX"
 
-echo "Launch celery"
+echo "*** Launch celery"
 export C_FORCE_ROOT=True
 exec celery -A async_server worker -l info
--- images/web/launch_gunicorn.sh
+++ images/web/launch_gunicorn.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-echo "RUNNING GUNICORN"
+echo "*** RUNNING GUNICORN"
 CODE_PATH="/home/$CONDA_USER/kge-server"
 cd $CODE_PATH
+
+echo "*** Installing deps"
 python3 setup.py install
+echo "*** end installing"
+
 DATASETS_PATH="$CODE_PATH/datasets/"
 export $DATASETS_PATH
 WORKING_DIR_EX="$CODE_PATH/rest-service/"
 cd $WORKING_DIR_EX
 echo "$WORKING_DIR_EX"
 
-echo "Launch gunicorn"
+echo "*** Launch gunicorn"
 exec gunicorn -b 0.0.0.0:8000 routes:app -w 4 --threads 4 --reload -t 120 --log-level debug --access-logfile '-'
