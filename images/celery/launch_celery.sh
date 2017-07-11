ed2d075b730da5a4a75a60258e0aee4d89fb2f22
Víctor Fernández Rico <vfrico@gmail.com>
2017-07-11 14:01:48 +0200

    Update docker deployment with dockerhub

--- images/celery/launch_celery.sh
+++ images/celery/launch_celery.sh
@@ -8,9 +8,12 @@ python3 setup.py install
 echo "*** end installing"
 
 WORKING_DIR_EX="$CODE_PATH/rest-service/"
+DATASETS_PATH="$CODE_PATH/datasets/"
 cd $WORKING_DIR_EX
 echo "$WORKING_DIR_EX"
 
 echo "*** Launch celery"
 export C_FORCE_ROOT=True
+export SQLITE_DATABASE_FILE_PATH="$DATASETS_PATH/server.db"
+export $DATASETS_PATH
 exec celery -A async_server worker -l info
--- images/web/launch_gunicorn.sh
+++ images/web/launch_gunicorn.sh
@@ -7,11 +7,12 @@ echo "*** Installing deps"
 python3 setup.py install
 echo "*** end installing"
 
-DATASETS_PATH="$CODE_PATH/datasets/"
-export $DATASETS_PATH
 WORKING_DIR_EX="$CODE_PATH/rest-service/"
+DATASETS_PATH="$CODE_PATH/datasets/"
 cd $WORKING_DIR_EX
 echo "$WORKING_DIR_EX"
 
 echo "*** Launch gunicorn"
+export SQLITE_DATABASE_FILE_PATH="$DATASETS_PATH/server.db"
+export $DATASETS_PATH
 exec gunicorn -b 0.0.0.0:8000 routes:app -w 4 --threads 4 --reload -t 120 --log-level debug --access-logfile '-'
